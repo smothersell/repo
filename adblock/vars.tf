6e037be8255b03c681f3df0f586eaa51cec3f68e
smothersell <126709043+smothersell@users.noreply.github.com>
2023-03-16 04:43:27 -0400

    Add files via upload

---
+++ adblock/vars.tf
@@ -0,0 +1,18 @@
+variable "cloudflare_email" {
+  type = string
+  default = ""
+}
+
+variable "cloudflare_api_key" {
+  type = string
+  default = ""
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+  default = ""
+}
+variable "cloudflare_account_id" {
+  type = string
+  default = ""
+}
