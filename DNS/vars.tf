e4b96ad5027ad84bf616b7f65bb17ab9d2985d51
smothersell <126709043+smothersell@users.noreply.github.com>
2023-03-16 05:04:53 -0400

    Add files via upload

---
+++ DNS/vars.tf
@@ -0,0 +1,14 @@
+variable "cloudflare_email" {
+  type = string
+  default = ""
+}
+
+variable "cloudflare_api_key" {
+  type = string
+  default = ""
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+  default = ""
+}
